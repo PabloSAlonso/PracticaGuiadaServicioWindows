36b868f1994e7ce1e94f60b81c3d5ffa9c85fcac
PabloSAlonso <santanaalonsopablo@gmail.com>
2026-01-29 12:24:58 +0100

    changes

--- ServicioFechaHoraWindows/ServicioWindows.Designer.cs
+++ ServicioFechaHoraWindows/ServicioWindows.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ServicioFechaHoraWindows
 {
-    partial class Service1
+    partial class ServicioWindows
     {
         /// <summary> 
         /// Variable del diseñador necesaria.
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.ServiceName = "Service1";
+            // 
+            // ServicioWindows
+            // 
+            this.ServiceName = "ServicioWindows";
+
         }
 
         #endregion
--- ServicioFechaHoraWindows/ServicioWindows.cs
+++ ServicioFechaHoraWindows/ServicioWindows.cs
@@ -10,9 +10,9 @@ using System.Threading.Tasks;
 
 namespace ServicioFechaHoraWindows
 {
-    public partial class Service1 : ServiceBase
+    public partial class ServicioWindows : ServiceBase
     {
-        public Service1()
+        public ServicioWindows()
         {
             InitializeComponent();
         }
